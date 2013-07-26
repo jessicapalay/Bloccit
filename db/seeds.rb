9611bff1bf258d3bafa8d3804ef12ab832cee164
Jessica Palay <jessica.c.palay@gmail.com>
2013-07-26 00:06:58 -0500

    Added comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,55 @@
 require 'faker'
 
-#Create 15 topics
-topics = []
-15.times do
-  topics << Topic.create(
-    name: Faker::Lorem.words(rand(1..10)).join( " "),
-    description: Faker::Lorem.paragraph(rand(1..4))
-    )
-end
+  #Create 15 topics
+  topics = []
+  15.times do
+    topics << Topic.create(
+      name: Faker::Lorem.words(rand(1..10)).join( " "),
+      description: Faker::Lorem.paragraph(rand(1..4))
+      )
+  end
   
-rand(4..10).times do
-  password = Faker::Lorem.characters(10)
-  u = User.new(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    password: password,
-    password_confirmation: password)
-  u.skip_confirmation!
-  u.save
+  rand(4..10).times do
+    password = Faker::Lorem.characters(10)
+    u = User.new(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: password,
+      password_confirmation: password)
+    u.skip_confirmation!
+    u.save
+ 
 
-rand(5..12).times do
-  topic = topics.first #getting the first topic here
-  p = u.posts.create(
-    title: Faker::Lorem.words(rand(1..10)).join(" "),
-    body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
-  # set the created_at to a time within the past year
-  p.update_attribute(:created_at, Time.now - rand(600..31536000))
+  rand(5..12).times do
+    topic = topics.first #getting the first topic here
+    p = u.posts.create(
+      title: Faker::Lorem.words(rand(1..10)).join(" "),
+      body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+    # set the created_at to a time within the past year
+    p.update_attribute(:created_at, Time.now - rand(600..31536000))
+  
 
-  topics.rotate! #add this line to move the first topic to the last, so that posts get assigned to different topics.
+    topics.rotate! #add this line to move the first topic to the last, so that posts get assigned to different topics.
 
-rand(3..7).times do
-  p.comments.create(
-    body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+    rand(3..7).times do
+      p.comments.create(
+      body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+    end
+  end
+
+
+  post_count = Post.count
+  User.all.each do |user|
+    rand(30..50).times do
+      p = Post.find(rand(1..post_count))
+      c = user.comments.create(
+        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
+        post: p)
+      c.update_attribute(:created_at, Time.now - rand(600..31536000))
+   
+    end
   end
 end
-end
-
 
 u = User.new(
   name: 'Admin User',
