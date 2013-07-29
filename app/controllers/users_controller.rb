92668f6b69dc42c9ec94c06d027a0e3278b20461
Jessica Palay <jessica.c.palay@gmail.com>
2013-07-28 23:14:26 -0500

    Created Popular Posts and Users, and optimized topics#show with eager loading.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
     @posts = @user.posts.public(current_user)  
   end
 
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+  end
+
 end
